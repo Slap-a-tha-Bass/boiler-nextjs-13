b16095c85fd5e4d9680d0a2069699171b28d09c3
Slap-a-tha-Bass <cdeloach16@gmail.com>
2022-10-28 22:18:20 -0500

    build: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import fs from 'fs';
+import matter from 'gray-matter';
+
+async function getData() {
+  const files = fs.readdirSync('src/posts');
+  const posts = files.map((filename) => {
+    const markdownWithMetadata = fs
+      .readFileSync(`src/posts/${filename}`)
+      .toString();
+    const { data: posts } = matter(markdownWithMetadata);
+    return {
+      posts,
+    };
+  });
+  return posts;
+}
+
+export default async function Page() {
+  const data = await getData();
+  return data.map((blog) => (
+    <div key={blog.posts.title}>
+      <h1>{blog.posts.title}</h1>
+      <p>{blog.posts.date}</p>
+    </div>
+  ));
 }
